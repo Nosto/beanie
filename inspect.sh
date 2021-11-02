45124999c9e92d05d120ddcd2e4677356b3fdeef
Mridang Agarwalla <mridang@nosto.com>
2021-11-02 08:58:52 +0200

    Updated the inspection script.

--- inspect.sh
+++ inspect.sh
@@ -10,4 +10,4 @@
 #  Nosto Solutions Ltd.
 #
 
-docker run --env GITHUB_WORKSPACE=/app --volume="$(pwd)"/.:/app docker.pkg.github.com/mridang/action-idea/idealize:2020.3.3 /app /app/.idea/inspectionProfiles/CI.xml /out v2 Inspection
+docker run --env GITHUB_WORKSPACE=/app --volume="$(pwd)"/.:/app docker.pkg.github.com/mridang/action-idea/idealize:2020.3.3 /app /app/.idea/inspectionProfiles/CI.xml /out v2 Inspection noop 1347
